9f78f29b5617390156bb8824ab987cf8c3035d96
Juan dos Santos Alonso <88332410+alonsojj@users.noreply.github.com>
2025-01-31 17:33:52 +0000

    makes database.js more rubust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,5 @@
 import { Client } from "pg";
 
-
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
@@ -9,21 +8,26 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
-  try{
+  console.log(
+    "Postgres's client credentials: ",
+      {
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        database: process.env.POSTGRES_DB,
+        password: process.env.POSTGRES_PASSWORD,
+      },
+  );
+  try {
+    await client.connect();
     const result = await client.query(queryObject);
-    return result
-  }
-  catch(error){
-
-  }
-  finally{
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error
+  } finally {
     await client.end();
   }
-  
-  
-  
-  ;
 }
 
 export default {
