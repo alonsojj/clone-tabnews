3064a525c4d9fb0b072dc939b356e878651e169b
Juan dos Santos Alonso <88332410+alonsojj@users.noreply.github.com>
2025-01-31 18:30:41 +0000

    handles  database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,24 +7,22 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV=="development" ? false:true,
   });
-  console.log(
-    "Postgres's client credentials: ",
-      {
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-      },
-  );
+  console.log("Postgres's client credentials: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
-    throw error
+    throw error;
   } finally {
     await client.end();
   }
